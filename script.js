35a67c0e47b3c94f2346d292396c619afe7f8b8a
ivktya <32465921+ivktya@users.noreply.github.com>
2026-04-27 22:19:24 +0200

    push push push

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function colorChange(tabId) {
-    let tab = document.getElementById(tabId);
-    
-    document.body.style.backgroundColor = tabId.backgroundColor;
-}
+document.addEventListener("DOMContentLoaded", () => {
 
+    'use strict';
+    
+    const tabs = document.querySelectorAll('.tab');
+    const boxes = document.querySelectorAll('.box');
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            const i = parseInt(tab.dataset.index);
+            
+            boxes.forEach((b, j) => b.classList.toggle('hidden', j !== i));
+        });
+    });
+});
